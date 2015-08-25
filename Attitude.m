a925e702d70720f150f640b413a233b162d59221
Pelatos <mattia.giurato@gmail.com>
2015-08-25 22:30:29 +0300

    Devo introdurre la media su una finestra fissa in modo da ridurre il numero di campioni dell'optitrack mediando

--- Attitude.m
+++ Attitude.m
@@ -21,9 +21,15 @@ gyro = RAW(:,5:7);
 mag = RAW(:,8:10);
 
 %Calibrate sensors
-Accelerometer = acc * diag(gain_a) - ones(length(acc),3)*diag(bias_a);
-Gyroscope = (gyro - ones(length(gyro),3)*diag(gyro(1,:))) * 0.001;
-Magnetometer = mag * diag(gain_m) - ones(length(mag),3)*diag(bias_m);
+Accelerometer(:,1) = gain_a(1) * acc(:,1) - bias_a(1);
+Accelerometer(:,2) = gain_a(2) * acc(:,2) - bias_a(2);
+Accelerometer(:,3) = gain_a(3) * acc(:,3) - bias_a(3);
+Gyroscope(:,1) = (gyro(:,1) - gyro(1,1))/1000;
+Gyroscope(:,2) = (gyro(:,2) - gyro(1,2))/1000;
+Gyroscope(:,3) = (gyro(:,3) - gyro(1,3))/1000;
+Magnetometer(:,1) = gain_m(1) * mag(:,1) - bias_m(1);
+Magnetometer(:,2) = gain_m(2) * mag(:,2) - bias_m(2);
+Magnetometer(:,3) = gain_m(3) * mag(:,3) - bias_m(3);
 
 %Import Optitrack data
 filename = '/home/pela/Documents/MATLAB/Attitude_Estimation/log_opti.txt';
@@ -45,17 +51,33 @@ orientationy = dataArray{:, 9};
 orientationz = dataArray{:, 10};
 orientationw = dataArray{:, 11};
 OPTIquaternion = [orientationw orientationx orientationy orientationz];
-OPTIeuler = quatern2euler(quaternConj(OPTIquaternion)) * (180/pi);
 
 clearvars RAW filename delimiter startRow formatSpec fileID dataArray ans stamp;
 
+%% ifhnn2f 
+IMUsample = mean(diff(IMUtime));
+OPTIsample = mean(diff(OPTItime));
+IMUstart = 136;
+OPTIstart = 530;
+IMUend = length(IMUtime);
+OPTIend = length(OPTItime);
+
+IMUtime = IMUtime(IMUstart:IMUend) - IMUtime(IMUstart);
+Accelerometer = Accelerometer(IMUstart:IMUend, :);
+Gyroscope = Gyroscope(IMUstart:IMUend, :);
+Magnetometer = Magnetometer(IMUstart:IMUend, :);
+
+OPTItime = downsample(OPTItime(OPTIstart:OPTIend),floor(IMUsample/OPTIsample)) - OPTItime(OPTIstart);
+OPTIquaternion = downsample(OPTIquaternion(OPTIstart:OPTIend, :),floor(IMUsample/OPTIsample));
+
 %% Process sensor data through Madgwick algorithm
 
-AHRS = MadgwickAHRS('SamplePeriod', 0.04, 'Beta', 0.1);
+AHRS = MadgwickAHRS('SamplePeriod', IMUsample, 'Beta', 0.12);
 
 IMUquaternion = zeros(length(IMUtime), 4);
 for t = 1:length(IMUtime)
-    AHRS.Update(Gyroscope(t,:) * (pi/180), Accelerometer(t,:), Magnetometer(t,:));	% gyroscope units must be radians
+    AHRS.UpdateIMU(Gyroscope(t,:) * (pi/180), Accelerometer(t,:));	% gyroscope units must be radians
+    %AHRS.Update(Gyroscope(t,:) * (pi/180), Accelerometer(t,:), Magnetometer(t,:));	% gyroscope units must be radians
     IMUquaternion(t, :) = AHRS.Quaternion;
 end
 
@@ -66,28 +88,29 @@ end
 % See: http://en.wikipedia.org/wiki/Gimbal_lock
 
 IMUeuler = quatern2euler(quaternConj(IMUquaternion)) * (180/pi);	% use conjugate for sensor frame relative to Earth and convert to degrees.
+OPTIeuler = quatern2euler(quaternConj(OPTIquaternion)) * (180/pi);
 
-figure('Name', 'IMU - Euler Angles');
-hold on;
-plot(IMUtime, IMUeuler(:,1), 'r');
-plot(IMUtime, IMUeuler(:,2), 'g');
-plot(IMUtime, IMUeuler(:,3), 'b');
-title('IMU - Euler angles');
-xlabel('Time (s)');
-ylabel('Angle (deg)');
-legend('\phi', '\theta', '\psi');
-hold off;
-
-figure('Name', 'OPTITRACK - Euler Angles');
-hold on;
-plot(OPTItime, OPTIeuler(:,1), 'r');
-plot(OPTItime, OPTIeuler(:,2), 'g');
-plot(OPTItime, OPTIeuler(:,3), 'b');
-title('OPTI - Euler angles');
-xlabel('Time (s)');
-ylabel('Angle (deg)');
-legend('\phi', '\theta', '\psi');
-hold off;
+% figure('Name', 'IMU - Euler Angles');
+% hold on;
+% plot(IMUtime, IMUeuler(:,1), 'r');
+% plot(IMUtime, IMUeuler(:,2), 'g');
+% plot(IMUtime, IMUeuler(:,3), 'b');
+% title('IMU - Euler angles');
+% xlabel('Time (s)');
+% ylabel('Angle (deg)');
+% legend('\phi', '\theta', '\psi');
+% hold off;
+% 
+% figure('Name', 'OPTITRACK - Euler Angles');
+% hold on;
+% plot(OPTItime, OPTIeuler(:,1), 'r');
+% plot(OPTItime, OPTIeuler(:,2), 'g');
+% plot(OPTItime, OPTIeuler(:,3), 'b');
+% title('OPTI - Euler angles');
+% xlabel('Time (s)');
+% ylabel('Angle (deg)');
+% legend('\phi', '\theta', '\psi');
+% hold off;
 
 figure('Name', 'IMU - Quaternions');
 hold on;
@@ -95,7 +118,7 @@ plot(IMUtime, IMUquaternion(:,1));
 plot(IMUtime, IMUquaternion(:,2));
 plot(IMUtime, IMUquaternion(:,3));
 plot(IMUtime, IMUquaternion(:,4));
-title('IMU - Euler angles');
+title('IMU - Quaternions');
 xlabel('Time (s)');
 ylabel('Quaternion');
 legend('w','x','y','z');
@@ -107,7 +130,7 @@ plot(OPTItime, OPTIquaternion(:,1));
 plot(OPTItime, OPTIquaternion(:,2));
 plot(OPTItime, OPTIquaternion(:,3));
 plot(OPTItime, OPTIquaternion(:,4));
-title('OPTI - Euler angles');
+title('OPTI - Quaternions');
 xlabel('Time (s)');
 ylabel('Quaternion');
 legend('w','x','y','z');
